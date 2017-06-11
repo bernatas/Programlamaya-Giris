f0158ba1c5f897ba82eceae15d2065822179fabe
Berna Taş <tasberna98@gmail.com>
2017-06-11 18:34:25 +0300

    C# dilinde ekrana Merhaba Dünya yazan kod

---
+++ MerhabaDünya.cs
@@ -0,0 +1,11 @@
+class Program
+	{
+		 static void Main(string[] args)
+		{
+			//Ekrana yazı yazdırmak için Console.Write komutu kullanılmaktadır. 
+        Console.WriteLine("Merhaba Dünya"); 
+        Console.ReadLine();
+			
+		}
+	}
+
